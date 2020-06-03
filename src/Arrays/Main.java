7855ac9151da40fb6ab4b3ae763a274ab519d6f6
Jihun Park <jxxparker@gmail.com>
2020-06-03 17:50:01 -0500

    array for loop

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -4,8 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] myVariable = new int[10];
+		int[] myIntArray = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 		
-	}
+		for(int i = 0; i < myIntArray.length; i++) {
+			myIntArray[i] = myIntArray[i] * 10;
+			System.out.println("element " + i + " value is " + myIntArray[i]);
+		}
 
+	}
+	
+	public static void printArray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			array[i] = array[i] * 10;
+			System.out.println("element " + i + " value is " + array[i]);
+		}
+	}
+		
 }
+
+
